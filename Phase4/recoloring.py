625fa8fa2695a7077d5a98ea376c2bbfd38b155f
Akshay Rao <71383128+akshay-rao7@users.noreply.github.com>
2020-10-05 23:25:09 +0530

    Add files via upload

---
+++ Phase4/recoloring.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+img = np.zeros((512,512,3),np.uint8)
+
+#colouring image
+#img[:]=255,0,0
+img[200:300,100:300]=255,0,0
+
+cv2.imshow("Image",img)
+
+cv2.waitKey(0)

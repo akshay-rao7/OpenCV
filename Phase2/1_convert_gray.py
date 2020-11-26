914fad6d893a42fbf101453cceb8666ff3144995
Akshay Rao <71383128+akshay-rao7@users.noreply.github.com>
2020-11-27 01:38:46 +0530

    Update 1_convert_gray.py

--- Phase2/1_convert_gray.py
+++ Phase2/1_convert_gray.py
@@ -1,11 +1,11 @@
 import cv2
 
-source_path = "D:\Workspace\code_space\Computer Vision\Computer Vision 1.1\img1.jpg"
+source_path = "D:\Workspace\code_space\Computer Vision_old\Computer Vision 1.2\sunflower.jpg"
 
 img = cv2.imread(source_path)
 
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Normal",img)
-cv2.imshow("Gray",imgGray)
+cv2.imshow("Color image",img)
+cv2.imshow("Grayscale image",imgGray)
 cv2.waitKey(0)

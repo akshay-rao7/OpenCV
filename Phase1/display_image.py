dd7a940a2a3ff06e7c3347c31524b5ee0b03bed8
Akshay Rao <71383128+akshay-rao7@users.noreply.github.com>
2020-11-27 01:31:14 +0530

    Update display_image.py

--- Phase1/display_image.py
+++ Phase1/display_image.py
@@ -1,8 +1,10 @@
-import cv2 #importing opencv package
+import cv2 #importing package
 
-path = 'D:\Workspace\code_space\OpenCV_tutorial\img1.jpg' #mention the path from where the image will be read
+path = r'D:\Workspace\code_ground\OpenCV\1.image_webcam_video\1.1display_image\sunflower.jpg' #path where the image is saved
 
-image = cv2.imread(path) #reading the image
+image = cv2.imread(path,1) #reading the image, 0 -> grayscale , 1 -> color image
 
-cv2.imshow("surprise",image)  #display the image window labelled "surprise"
-cv2.waitKey(0) #continue displaying it 
+cv2.imshow("Flower",image) #Displaying the image with the label "Flower"
+cv2.waitKey(0) # 0 = infinite time | value = (second*1000)
+
+cv2.destroyAllWindows() #Closes all windows

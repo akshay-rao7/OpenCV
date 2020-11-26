3adf334c358763ed9315ce1f95a6564730a9c2d9
Akshay Rao <71383128+akshay-rao7@users.noreply.github.com>
2020-11-27 02:03:32 +0530

    Update 2_blur.py

--- Phase2/2_blur.py
+++ Phase2/2_blur.py
@@ -1,6 +1,6 @@
 import cv2
 
-source_path = "D:\Workspace\code_space\Computer Vision\Computer Vision 1.1\img1.jpg"
+source_path = "D:\Workspace\code_space\Computer Vision_old\Computer Vision 1.2\sunflower.jpg"
 
 img = cv2.imread(source_path)
 
@@ -8,7 +8,7 @@ imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
 #(7,7) = always odd
 #sigmaX = 0
-cv2.imshow("Normal",img)
-cv2.imshow("Gray image",imgGray)
-cv2.imshow("Gaussian blur",imgBlur)
+cv2.imshow("Normal Color image",img)
+cv2.imshow("Grayscale image",imgGray)
+cv2.imshow("Gaussian blur image",imgBlur)
 cv2.waitKey(0)

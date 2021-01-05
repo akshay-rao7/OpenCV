1665eff8b859f28469b6eb7024a07a7f03ad5d4d
Akshay Rao <71383128+akshay-rao7@users.noreply.github.com>
2021-01-05 17:20:23 +0530

    Update display_image.py

--- Phase1/display_image.py
+++ Phase1/display_image.py
@@ -1,10 +1,10 @@
 import cv2 #importing package
 
-path = r'D:\Workspace\code_ground\OpenCV\1.image_webcam_video\1.1display_image\sunflower.jpg' #path where the image is saved
+path = 'D:\code_base\lenna.jpg' #path where the image is saved
 
 image = cv2.imread(path,1) #reading the image, 0 -> grayscale , 1 -> color image
 
-cv2.imshow("Flower",image) #Displaying the image with the label "Flower"
-cv2.waitKey(0) # 0 = infinite time | value = (second*1000)
+cv2.imshow("Title",image) #Displaying the image with the label "Lenna"
+cv2.waitKey(2000) # 0 = infinite time | value = (second*1000)
 
 cv2.destroyAllWindows() #Closes all windows
